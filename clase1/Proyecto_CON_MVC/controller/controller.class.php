5b7b1b8c424ed85c6562b2b9998d0b05c0d9b1c1
estefanoshin <estefanoshin@yahoo.com.ar>
2018-04-06 19:49:10 -0300

    first commit

---
+++ clase1/Proyecto_CON_MVC/controller/controller.class.php
@@ -0,0 +1,8 @@
+<?php
+class Controller {
+
+	function mostrarResultados () {
+		echo 'Resultado';
+	}
+}
+?>
---
+++ clase1/Proyecto_CON_MVC/index.php
@@ -0,0 +1,3 @@
+<?php
+ require_once __DIR__.'controller/controller.class.php'; //__DIR__ hace que trabaje directament en donde esta posicionado el documento actual
+?>
